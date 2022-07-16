6b9169287b73b74bfc1538902ae6d901107d8ea3
Nicholas Gorden <ngorden1997@gmail.com>
2022-07-16 15:59:15 -0700

    fix: remove old configs

--- nvim/lua/configure_globals.lua
+++ nvim/lua/configure_globals.lua
@@ -1,9 +1,5 @@
 vim.g.using_chadtree = 1
 vim.g.using_cmp_nvim = 1
-vim.g.using_nerdtree = 0
-vim.g.using_lightline = 0
 vim.g.using_lsp_signature = 1
 vim.g.using_lualine = 1
 vim.g.using_lua_snip = 1
-vim.g.using_ultisnips = 0
-vim.g.using_vsnip = 0
